d7e4412903c4b1d5486007bce74d4aca1bb2ebda
Harshit Gupta <109791319+theharshit2202@users.noreply.github.com>
2024-07-11 16:24:19 +0530

    Create Do you know Table?! 1.java

---
+++ Do you know Table?! 1.java
@@ -0,0 +1,42 @@
+/*
+Help Lakshya Bhaiya recite tables of an input number x to the limit y.
+
+Print all the multiples of x till y.
+
+Note : y is inclusive
+
+Input Format
+Take two inputs, x and y
+
+Constraints
+1<=x<=10,000 1<=y<=10,000
+
+Output Format
+each line contains the table of the number x
+
+Sample Input
+13 50
+Sample Output
+13
+26
+39
+Explanation
+TABLE!!!!!!!!!
+*/
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt();
+        int y = scn.nextInt();
+
+        for (int i = 1; i * x <= y; i++) {
+            System.out.println(i * x);
+        }
+
+        scn.close();  // Close the scanner
+    }
+
+}
